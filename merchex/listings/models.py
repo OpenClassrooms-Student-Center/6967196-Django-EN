48cf1c901d38daff933502a00d23c2cf1f1a5923
patrickh <patrickh@PHs-Macbook-Air.local>
2021-03-30 01:04:21 +0100

    End of P3C1 code-along

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,23 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+       validators=[MinValueValidator(1900),
+                   MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 
 class Listing(models.Model):
