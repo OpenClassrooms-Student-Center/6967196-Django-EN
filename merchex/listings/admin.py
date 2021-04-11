e9214776baabc0a7d23ddeab754dbf391ebd551a
patrickh <patrickh@PHs-Macbook-Air.local>
2021-04-11 18:45:01 +0100

    End of P3C2 exercise

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from listings.models import Band
+from listings.models import Band, Listing
 
 
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
 admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
