601a910470fbe3a565ad36aa32a3a3d6f977a5dc
patrickh <patrickh@PHs-Macbook-Air.local>
2021-04-19 23:34:26 +0100

    P4C2 perform an action when all form fields are valid

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -41,6 +42,17 @@ def contact(request):
     if request.method == 'POST':
         # create an instance of our form, and fill it with the POST data
         form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonymous"} via MerchEx Contact Us form',
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+        # if the form is not valid, we let execution continue to the return
+        # statement below, and display the form again (with errors).
+
     else:
         # this must be a GET request, so create an empty form
         form = ContactUsForm()
