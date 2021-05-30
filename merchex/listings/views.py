8cd99cc98984c8b983d57acf3906d1cbccb8797e
patrickh <patrickh@PHs-Macbook-Air.local>
2021-05-30 13:14:05 +0100

    End of P2C2 code-along

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
