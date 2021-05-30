3202a8254d49c62683ac95bc4e89fbeb65d934ad
patrickh <patrickh@PHs-Macbook-Air.local>
2021-05-30 16:22:38 +0100

    End of P2C3 exercise

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from listings.models import Band
+from listings.models import Band, Listing
 
 
 def hello(request):
@@ -22,7 +22,17 @@ def about(request):
 
 
 def listings(request):
-    return HttpResponse('<h1>Listings</h1> <p>Check out the latest listings below!</p>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Listings</h1>
+        <p>Check out the latest listings below!</p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
+    """)
 
 
 def contact(request):
