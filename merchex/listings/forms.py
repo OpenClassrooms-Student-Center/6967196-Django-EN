3d08bf6330b3ddf670174e4e8881ca661daf8edc
patrickh <patrickh@PHs-Macbook-Air.local>
2021-06-06 12:30:08 +0100

    P4C3 exclude fields from a ModelForm

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -12,4 +12,4 @@ class ContactUsForm(forms.Form):
 class BandForm(forms.ModelForm):
     class Meta:
         model = Band
-        fields = '__all__'
+        exclude = ('active', 'official_homepage')
